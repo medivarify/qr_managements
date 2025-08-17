4a63e998a07e23693692f5eff8770b1a5943fffb
qprintnde-png <qprintnde@gmail.com>
2025-08-17 10:08:35 +0600

    Fix Arduino Cloud proxy SSL connection

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         target: 'https://api.arduino.cc',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/arduino/, ''),
-        secure: true,
+        secure: false,
         headers: {
           'Origin': 'https://api.arduino.cc'
         }
